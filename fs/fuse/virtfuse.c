jeevandeepj994/anolis_perf

anolis: virtfuse: improve mntpoint printing
ANBZ: #7794

Introduce a specialized version of prepend_path() to improve the
mntpoint printing:

1. print the full path of the mntpoint
    1) mount ext4 on /mnt
    2) mount fuse on /mnt/dir through virtfuse
    3) prior this patch, VIRTFUSE_IOC_GET_MOUNTS ioctl prints the mntpoint
       as "/dir", as dentry_path_raw() only prints the relative path of
       the dentry in which mount it resides in.
    4) with this patch, VIRTFUSE_IOC_GET_MOUNTS ioctl prints the
       absolute path of the mntpoint as "/mnt/dir"

2. print the right path of the mntpoint if it resides in a bind mount
    1) bind mount /foo/bar to /mnt
    2) mount fuse on /mnt/dir through virtfuse
    3) prior this patch, VIRTFUSE_IOC_GET_MOUNTS ioctl prints the mntpoint
       as "/foo/bar/dir"
    4) with this patch, VIRTFUSE_IOC_GET_MOUNTS ioctl prints the right
       path as "/mnt/dir"

Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>

fs/fuse/virtfuse.c
@@ -272,6 +272,11 @@ static int virtfuse_get_mounts(struct file *file, unsigned long arg)
 
 	lock_mount_hash();
 	list_for_each_entry(mnt, &sb->s_mounts, mnt_instance) {
+		struct path path = {
+			.dentry = mnt->mnt.mnt_root,
+			.mnt = &mnt->mnt
+		};
+
 		/* skip slave mounts */
 		if (mnt->mnt_master)
 			continue;
@@ -281,7 +286,7 @@ static int virtfuse_get_mounts(struct file *file, unsigned long arg)
 			continue;
 
 		/* mountpoint */
-		p = dentry_path_raw(mnt->mnt_mountpoint, name, PATH_MAX);
+		p = prepend_path_locked(&path, name, PATH_MAX);
 		if (IS_ERR(p)) {
 			ret = PTR_ERR(p);
 			break;

include/linux/dcache.h
@@ -308,6 +308,7 @@ extern char *d_absolute_path(const struct path *, char *, int);
 extern char *d_path(const struct path *, char *, int);
 extern char *dentry_path_raw(struct dentry *, char *, int);
 extern char *dentry_path(struct dentry *, char *, int);
+extern char *prepend_path_locked(const struct path *, char *, int);
 
 /* Allocation counts.. */
 

RevySR/linux

ANBZ: #8323
Introduce d_absolute_path_locked() to improve the mntpoint printing:
[jingbo's backport notes to ANCK 6.6]
Replace prepend_path_locked() with d_absolute_path_locked(), as we
have __prepend_path() helper now.
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2788

fs/fuse/virtfuse.c
@@ -275,6 +275,11 @@ static int virtfuse_get_mounts(struct file *file, unsigned long arg)
 	write_seqlock(&mount_lock);
 
 	list_for_each_entry(mnt, &sb->s_mounts, mnt_instance) {
+		struct path path = {
+			.dentry = mnt->mnt.mnt_root,
+			.mnt = &mnt->mnt
+		};
+
 		/* skip slave mounts */
 		if (mnt->mnt_master)
 			continue;
@@ -284,7 +289,7 @@ static int virtfuse_get_mounts(struct file *file, unsigned long arg)
 			continue;
 
 		/* mountpoint */
-		p = dentry_path_raw(mnt->mnt_mountpoint, name, PATH_MAX);
+		p = d_absolute_path_locked(&path, name, PATH_MAX);
 		if (IS_ERR(p)) {
 			ret = PTR_ERR(p);
 			break;

include/linux/dcache.h
@@ -296,6 +296,7 @@ extern char *d_absolute_path(const struct path *, char *, int);
 extern char *d_path(const struct path *, char *, int);
 extern char *dentry_path_raw(const struct dentry *, char *, int);
 extern char *dentry_path(const struct dentry *, char *, int);
+extern char *d_absolute_path_locked(const struct path *, char *, int);
 
 /* Allocation counts.. */
 